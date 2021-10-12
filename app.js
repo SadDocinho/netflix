2b224dd96f41600eea67d46697eb981cf17f8b95
Pedro Henrique <phcpedro177@hotmail.com>
2021-10-12 20:23:07 -0300

    Add a gitignore file and make a express server.

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+let port = 3000;
+
+app.use('/', express.static(__dirname))
+
+app.listen(port, () => {
+    console.log(`O servidor express está rodando na porta: ${port}`)
+})

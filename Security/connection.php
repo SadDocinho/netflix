534c0db2d986820c0b6d26f55f1faa3ea27da80d
Felipe Monteiro <67602498+SadDocinho@users.noreply.github.com>
2022-11-20 17:31:48 -0300

    a

--- Security/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST', '127.0.0.1');
-define('USER', 'root');
-define('PASSWORD', '');
-define('DB', 'login');
-
-$connection = mysqli_connect(HOST, USER, PASSWORD, DB) or die ('Não conectou');
